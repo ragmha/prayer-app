3156462af2b959fed8f6c83b354de0fa54f05222
Räghib Hasan <raghibmidhat@gmail.com>
2024-02-04 15:37:08 +0200

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: ['universe/native'],
+  rules: {
+    'import/order': 0,
+    'react-native/no-inline-styles': 0,
+    'import/namespace': 0,
+    'no-duplicate-imports': 'error'
+  }
+}
--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,13 @@
+/* eslint-env node */
+
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
-});
+  __dirname,
+})
 
-module.exports = config;
+module.exports = config
